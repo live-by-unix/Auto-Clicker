b38155596a87cefbb51934fc83e60e3c3ef53334
live-by-unix <262261208+live-by-unix@users.noreply.github.com>
2026-06-18 19:21:27 -0700

    Refactor auto clicker to use new mouse coordinates

--- content.js
+++ content.js
@@ -2,29 +2,50 @@ if (!window.autoClickerInstalled) {
   window.autoClickerInstalled = true;
 
   let timer = null;
-  let mouseX = 0;
-  let mouseY = 0;
+  let x = 0;
+  let y = 0;
 
   document.addEventListener("mousemove", e => {
-    mouseX = e.clientX;
-    mouseY = e.clientY;
+    x = e.clientX;
+    y = e.clientY;
   });
 
-  chrome.runtime.onMessage.addListener(msg => {
+  chrome.runtime.onMessage.addListener((msg) => {
+
     if (msg.action === "start") {
       clearInterval(timer);
 
+      const clickX = x;
+      const clickY = y;
+
       timer = setInterval(() => {
-        const element = document.elementFromPoint(mouseX, mouseY);
-        if (element) {
-          element.click();
+        const el = document.elementFromPoint(clickX, clickY);
+
+        if (el) {
+          el.dispatchEvent(new MouseEvent("mousedown", {
+            bubbles: true,
+            clientX: clickX,
+            clientY: clickY
+          }));
+
+          el.dispatchEvent(new MouseEvent("mouseup", {
+            bubbles: true,
+            clientX: clickX,
+            clientY: clickY
+          }));
+
+          el.dispatchEvent(new MouseEvent("click", {
+            bubbles: true,
+            clientX: clickX,
+            clientY: clickY
+          }));
         }
       }, msg.interval);
     }
 
     if (msg.action === "stop") {
       clearInterval(timer);
-      timer = null;
     }
+
   });
 }

37811e6287b12b356756952cc9a726d5ddf85fbd
live-by-unix <262261208+live-by-unix@users.noreply.github.com>
2026-06-18 19:30:14 -0700

    Update content.js

--- content.js
+++ content.js
@@ -2,49 +2,59 @@ if (!window.autoClickerInstalled) {
   window.autoClickerInstalled = true;
 
   let timer = null;
-  let x = 0;
-  let y = 0;
+  let mouseX = null;
+  let mouseY = null;
 
   document.addEventListener("mousemove", e => {
-    x = e.clientX;
-    y = e.clientY;
+    mouseX = e.clientX;
+    mouseY = e.clientY;
   });
 
-  chrome.runtime.onMessage.addListener((msg) => {
+  chrome.runtime.onMessage.addListener(msg => {
 
     if (msg.action === "start") {
+
+      if (mouseX === null || mouseY === null) {
+        console.log("Move the mouse first.");
+        return;
+      }
+
+      const clickX = mouseX;
+      const clickY = mouseY;
+
       clearInterval(timer);
 
-      const clickX = x;
-      const clickY = y;
-
       timer = setInterval(() => {
+
         const el = document.elementFromPoint(clickX, clickY);
 
-        if (el) {
-          el.dispatchEvent(new MouseEvent("mousedown", {
-            bubbles: true,
-            clientX: clickX,
-            clientY: clickY
-          }));
+        if (!el) return;
 
-          el.dispatchEvent(new MouseEvent("mouseup", {
-            bubbles: true,
-            clientX: clickX,
-            clientY: clickY
-          }));
+        el.dispatchEvent(new MouseEvent("mousedown", {
+          bubbles: true,
+          clientX: clickX,
+          clientY: clickY
+        }));
 
-          el.dispatchEvent(new MouseEvent("click", {
-            bubbles: true,
-            clientX: clickX,
-            clientY: clickY
-          }));
-        }
+        el.dispatchEvent(new MouseEvent("mouseup", {
+          bubbles: true,
+          clientX: clickX,
+          clientY: clickY
+        }));
+
+        el.dispatchEvent(new MouseEvent("click", {
+          bubbles: true,
+          clientX: clickX,
+          clientY: clickY
+        }));
+
       }, msg.interval);
+
     }
 
     if (msg.action === "stop") {
       clearInterval(timer);
+      timer = null;
     }
 
   });
